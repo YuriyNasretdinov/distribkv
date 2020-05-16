8bf90608f5afc15f6a81ad4638b69b55aacc7fd4
Yuriy Nasretdinov <nasretdinov@gmail.com>
2020-05-16 21:01:30 +0300

    Changes made between part2 and part3 to simplify testing

--- web/web.go
+++ web/web.go
@@ -2,40 +2,30 @@ package web
 
 import (
 	"fmt"
-	"hash/fnv"
 	"io"
 	"net/http"
 
+	"github.com/YuriyNasretdinov/distribkv/config"
 	"github.com/YuriyNasretdinov/distribkv/db"
 )
 
 // Server contains HTTP method handlers to be used for the database.
 type Server struct {
-	db         *db.Database
-	shardIdx   int
-	shardCount int
-	addrs      map[int]string
+	db     *db.Database
+	shards *config.Shards
 }
 
 // NewServer creates a new instance with HTTP handlers to be used to get and set values.
-func NewServer(db *db.Database, shardIdx, shardCount int, addrs map[int]string) *Server {
+func NewServer(db *db.Database, s *config.Shards) *Server {
 	return &Server{
-		db:         db,
-		shardIdx:   shardIdx,
-		shardCount: shardCount,
-		addrs:      addrs,
+		db:     db,
+		shards: s,
 	}
 }
 
-func (s *Server) getShard(key string) int {
-	h := fnv.New64()
-	h.Write([]byte(key))
-	return int(h.Sum64() % uint64(s.shardCount))
-}
-
 func (s *Server) redirect(shard int, w http.ResponseWriter, r *http.Request) {
-	url := "http://" + s.addrs[shard] + r.RequestURI
-	fmt.Fprintf(w, "redirecting from shard %d to shard %d (%q)\n", s.shardIdx, shard, url)
+	url := "http://" + s.shards.Addrs[shard] + r.RequestURI
+	fmt.Fprintf(w, "redirecting from shard %d to shard %d (%q)\n", s.shards.CurIdx, shard, url)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -53,15 +43,15 @@ func (s *Server) GetHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	key := r.Form.Get("key")
 
-	shard := s.getShard(key)
+	shard := s.shards.Index(key)
 	value, err := s.db.GetKey(key)
 
-	if shard != s.shardIdx {
+	if shard != s.shards.CurIdx {
 		s.redirect(shard, w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "Shard = %d, current shard = %d, addr = %q, Value = %q, error = %v", shard, s.shardIdx, s.addrs[shard], value, err)
+	fmt.Fprintf(w, "Shard = %d, current shard = %d, addr = %q, Value = %q, error = %v", shard, s.shards.CurIdx, s.shards.Addrs[shard], value, err)
 }
 
 // SetHandler handles write requests from the database.
@@ -70,12 +60,12 @@ func (s *Server) SetHandler(w http.ResponseWriter, r *http.Request) {
 	key := r.Form.Get("key")
 	value := r.Form.Get("value")
 
-	shard := s.getShard(key)
-	if shard != s.shardIdx {
+	shard := s.shards.Index(key)
+	if shard != s.shards.CurIdx {
 		s.redirect(shard, w, r)
 		return
 	}
 
 	err := s.db.SetKey(key, []byte(value))
-	fmt.Fprintf(w, "Error = %v, shardIdx = %d, current shard = %d", err, shard, s.shardIdx)
+	fmt.Fprintf(w, "Error = %v, shardIdx = %d, current shard = %d", err, shard, s.shards.CurIdx)
 }
