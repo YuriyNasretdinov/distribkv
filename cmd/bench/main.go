815ff716969350db2adcc9a7ba6a7f20d4cb0e85
Yuriy Nasretdinov <nasretdinov@gmail.com>
2020-06-14 13:44:26 +0300

    Changes made in part 6

--- cmd/bench/main.go
+++ cmd/bench/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -12,19 +14,29 @@ import (
 )
 
 var (
-	addr        = flag.String("addr", "localhost:8080", "The HTTP host port for the instance that is benchmarked.")
-	iterations  = flag.Int("iterations", 1000, "The number of iterations for writing")
-	concurrency = flag.Int("concurrency", 1, "How many goroutines to run in parallel when doing writes")
+	addr           = flag.String("addr", "localhost:8080", "The HTTP host port for the instance that is benchmarked.")
+	iterations     = flag.Int("iterations", 1000, "The number of iterations for writing")
+	readIterations = flag.Int("read-iterations", 100000, "The number of iterations for reading")
+	concurrency    = flag.Int("concurrency", 1, "How many goroutines to run in parallel when doing writes")
 )
 
-func benchmark(name string, fn func()) {
+var httpClient = &http.Client{
+	Transport: &http.Transport{
+		IdleConnTimeout:     time.Second * 60,
+		MaxIdleConns:        300,
+		MaxConnsPerHost:     300,
+		MaxIdleConnsPerHost: 300,
+	},
+}
+
+func benchmark(name string, iter int, fn func() string) (qps float64, strs []string) {
 	var max time.Duration
 	var min = time.Hour
 
 	start := time.Now()
-	for i := 0; i < *iterations; i++ {
+	for i := 0; i < iter; i++ {
 		iterStart := time.Now()
-		fn()
+		strs = append(strs, fn())
 		iterTime := time.Since(iterStart)
 		if iterTime > max {
 			max = iterTime
@@ -34,24 +46,93 @@ func benchmark(name string, fn func()) {
 		}
 	}
 
-	avg := time.Since(start) / time.Duration(*iterations)
-	qps := float64(*iterations) / (float64(time.Since(start)) / float64(time.Second))
+	avg := time.Since(start) / time.Duration(iter)
+	qps = float64(iter) / (float64(time.Since(start)) / float64(time.Second))
 	fmt.Printf("Func %s took %s avg, %.1f QPS, %s max, %s min\n", name, avg, qps, max, min)
+
+	return qps, strs
 }
 
-func writeRand() {
-	key := fmt.Sprintf("key-%d", rand.Intn(1000000))
+func writeRand() (key string) {
+	key = fmt.Sprintf("key-%d", rand.Intn(1000000))
 	value := fmt.Sprintf("value-%d", rand.Intn(1000000))
 
 	values := url.Values{}
 	values.Set("key", key)
 	values.Set("value", value)
 
-	resp, err := http.Get("http://" + (*addr) + "/set?" + values.Encode())
+	resp, err := httpClient.Get("http://" + (*addr) + "/set?" + values.Encode())
 	if err != nil {
 		log.Fatalf("Error during set: %v", err)
 	}
+
+	io.Copy(ioutil.Discard, resp.Body)
 	defer resp.Body.Close()
+
+	return key
+}
+
+func readRand(allKeys []string) (key string) {
+	key = allKeys[rand.Intn(len(allKeys))]
+
+	values := url.Values{}
+	values.Set("key", key)
+
+	resp, err := httpClient.Get("http://" + (*addr) + "/get?" + values.Encode())
+	if err != nil {
+		log.Fatalf("Error during get: %v", err)
+	}
+	io.Copy(ioutil.Discard, resp.Body)
+	defer resp.Body.Close()
+
+	return key
+}
+
+func benchmarkWrite() (allKeys []string) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var totalQPS float64
+
+	for i := 0; i < *concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			qps, strs := benchmark("write", *iterations, writeRand)
+			mu.Lock()
+			totalQPS += qps
+			allKeys = append(allKeys, strs...)
+			mu.Unlock()
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	log.Printf("Write total QPS: %.1f, set %d keys", totalQPS, len(allKeys))
+
+	return allKeys
+}
+
+func benchmarkRead(allKeys []string) {
+	var totalQPS float64
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < *concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			qps, _ := benchmark("read", *readIterations, func() string { return readRand(allKeys) })
+			mu.Lock()
+			totalQPS += qps
+			mu.Unlock()
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	log.Printf("Read total QPS: %.1f", totalQPS)
 }
 
 func main() {
@@ -60,15 +141,8 @@ func main() {
 
 	fmt.Printf("Running with %d iterations and concurrency level %d\n", *iterations, *concurrency)
 
-	var wg sync.WaitGroup
+	allKeys := benchmarkWrite()
 
-	for i := 0; i < *concurrency; i++ {
-		wg.Add(1)
-		go func() {
-			benchmark("write", writeRand)
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
+	go benchmarkWrite()
+	benchmarkRead(allKeys)
 }
--- launch.sh
+++ launch.sh
@@ -10,9 +10,9 @@ sleep 0.1
 
 go install -v
 
-distribkv -db-location=moscow.db -http-addr=127.0.0.1:8080 -config-file=sharding.toml -shard=Moscow &
-distribkv -db-location=minsk.db -http-addr=127.0.0.1:8081 -config-file=sharding.toml -shard=Minsk &
-distribkv -db-location=kiev.db -http-addr=127.0.0.1:8082 -config-file=sharding.toml -shard=Kiev &
-distribkv -db-location=tashkent.db -http-addr=127.0.0.1:8083 -config-file=sharding.toml -shard=Tashkent &
+distribkv -db-location=moscow.db -http-addr=127.0.0.2:8080 -config-file=sharding.toml -shard=Moscow &
+# distribkv -db-location=minsk.db -http-addr=127.0.0.3:8080 -config-file=sharding.toml -shard=Minsk &
+# distribkv -db-location=kiev.db -http-addr=127.0.0.4:8080 -config-file=sharding.toml -shard=Kiev &
+# distribkv -db-location=tashkent.db -http-addr=127.0.0.5:8080 -config-file=sharding.toml -shard=Tashkent &
 
 wait
