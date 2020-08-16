cba34dc676604b4e60b3a3eb8f47bf451cd09287
Yuriy Nasretdinov <nasretdinov@gmail.com>
2020-08-16 12:04:11 +0000

    Fix populate script

--- populate.sh
+++ populate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for shard in localhost:8080 localhost:8081; do
+for shard in 127.0.0.2:8080; do
     echo $shard
-    for i in {1..1000}; do
+    for i in {1..10000}; do
         curl "http://$shard/set?key=key-$RANDOM&value=value-$RANDOM"
     done
 done
